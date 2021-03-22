acd4eed525f4bb5b34709501a9e0508ce5261c2e
sapatti <sapatti@pop-os.localdomain>
2021-03-22 22:01:17 +0200

    check notifier on update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-dunstify "updating" -r 4
+
+if [ -x "$(command -v dunstify)" ]; then
+    NOTIFIER="dunstify -r 4 "
+else
+    NOTIFIER="notify-send"
+fi
+
+$NOTIFIER "updating"
 sudo apt update && sudo apt upgrade -y && pkill -SIGRTMIN+10 i3blocks
-dunstify "update done" -r 4
+$NOTIFIER "update done"
 
