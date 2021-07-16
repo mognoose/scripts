efb8b9409157b3927fe10a379488d415930a78ed
Csaba <mognoose@gmail.com>
2021-07-16 12:37:13 +0300

    added nas mounting

---
+++ fileserver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="/mnt/fileserver"
+
+db="/home/$USER/Documents/keepass.kdbx"
+keyfile="/home/$USER/Documents/keepasskey"
+entry="NAS-Dashboard"
+
+#Get password from Keepass
+pass=$(keepassxc-cli show -sa password $db $entry --key-file $keyfile --no-password)
+
+#Create mount dir if it does not exist
+[[ -d "$dir" ]] || { echo "Dir not found...creating $dir"; sudo mkdir "$dir"; sudo chmod +rwx "$dir"; sudo chown $USER:$USER $dir;}
+
+#mount nas
+sshfs -o password_stdin admin@fs.csabaszilagyi.fi:/ $dir <<< $pass
