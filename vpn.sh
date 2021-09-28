1ed3219bdffac943421cc0f79add5973359ac62c
Csaba Szilagyi <mognoose@gmail.com>
2021-09-28 09:22:44 +0300

    vpn script to use keepass and more logic

--- vpn.sh
+++ vpn.sh
@@ -1,4 +1,24 @@
-VPN=$(nmcli con show --active | grep vpn )
-NAME=$(echo $VPN | awk '{print $1}')
+#!/bin/bash
+db="/home/$USER/Documents/keepass.kdbx"
+keyfile="/home/$USER/Documents/keepasskey"
+path="Oma/Codetag/vpn/"
 
-echo $NAME
+if [ $# -eq 0 ]
+  then
+    options=$(nmcli connection show | grep vpn | awk '{ print $1 }')
+    ans=$(echo "$options" | rofi -font 'lato 18' -dmenu -i -p "VPN")
+  else
+    ans=$1
+fi
+
+con="down"
+status=$(nmcli -f GENERAL.STATE connection show $ans)
+
+if [[ -z $status ]]
+  then
+    con="up"
+fi
+
+entry="$path$ans"
+pass=$(keepassxc-cli show -a Password $db $entry --key-file $keyfile --no-password)
+nmcli connection $con $ans --ask <<< $pass%   
