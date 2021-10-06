76091d7da9ba49ba702f7ed5c27ea7d1bbd8df7b
Csaba <mognoose@gmail.com>
2021-10-06 17:52:38 +0300

    added next and previous on scroll to np

--- np.sh
+++ np.sh
@@ -1,3 +1,8 @@
 np=$(playerctl --player spotify metadata --format '{{ artist }} - {{ title }}')
 status=$(playerctl --player spotify metadata --format '{{ status }}' | sed 's/Playing//g; s/Paused//g')
 echo $status $np
+case $BLOCK_BUTTON in
+    1) playerctl --player spotify play-pause ;;
+    4) playerctl --player spotify next ;;
+    5) playerctl --player spotify previous ;;
+esac
