a21342dd2c337525f5780b5c8a416c1aa31e4d2a
Csaba <csaba@lamia.fi>
2022-08-25 09:39:52 +0300

    fixed recognisation

--- touchpad.sh
+++ touchpad.sh
@@ -1,18 +1,31 @@
-natural=1
-tapping=1
-middlebutton=1
+if [ -z $1 ]
+  then
+    tap=1
+  else
+    tap=$1
+fi
+if [ -z $2 ]
+  then
+    nat=1
+  else
+    nat=$2
+fi
+if [ -z $3 ]
+  then
+    mid=1
+  else
+    mid=$3
+fi
 
 #Touchpad id
-id=$(xinput --list | grep Touch | awk {'print $6'} | sed 's/id=//g')
-#Tapping enabled
-tap=$(xinput --list-props $id | grep "Tapping Enabled (" | awk {'print $4'} | sed 's/(//g; s/)://g')
-#natural scrolling enabled
-nat=$(xinput --list-props $id | grep "Natural Scrolling Enabled (" | awk {'print $5'} | sed 's/(//g; s/)://g')
-#middlebutton emulation enabled
-mid=$(xinput --list-props $id | grep "Middle Emulation Enabled (" | awk {'print $5'} | sed 's/(//g; s/)://g')
-
-xinput --set-prop $id $tap $tapping &&
-xinput --set-prop $id $nat $natural &&
-xinput --set-prop $id $mid $middlebutton
-
+echo "Getting touchpad id"
+id=$(xinput --list | grep -m 1 Touch | awk {'print $6'} | sed 's/id=//g')
+echo "Touchpad id: $id"
+echo "setting props: tap:$tap, nat:$nat, mid:$mid"
+xinput --set-prop $id "libinput Tapping Enabled" $tap
+xinput --set-prop $id "libinput Natural Scrolling Enabled" $nat
+xinput --set-prop $id "libinput Middle Emulation Enabled" $mid
+xinput --list-props $id | grep "libinput Tapping Enabled ("
+xinput --list-props $id | grep "libinput Natural Scrolling Enabled ("
+xinput --list-props $id | grep "libinput Middle Emulation Enabled ("
 echo "done"
