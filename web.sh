65e494a74afb94b32727707571a742c0a54a8dfd
Csaba <csaba@lamia.fi>
2022-08-25 09:38:49 +0300

    added option to choose browser

--- web.sh
+++ web.sh
@@ -2,6 +2,11 @@
 DEFAULT="google-chrome"
 SEARCH="https://www.google.com/search?q="
 HOMEPAGE="https://google.com"
+declare -a BROWSERS
+BROWSERS=("google-chrome" "brave-browser" "firefox")
+BROWSERSNAMES="Chrome
+Brave
+Firefox"
 PAGES="Home
 reddit.com
 youtube.com
@@ -16,7 +21,8 @@ beta.protonmail.com
 calendar.google.com"
 
 if [ -z $1 ]; then
-	BROWSER=$DEFAULT
+	BROWSERINDEX=$(echo "$BROWSERSNAMES" | rofi -font 'lato 18' -dmenu -i -p "Browser:" -format i)
+    BROWSER=${BROWSERS[$BROWSERINDEX]}
 else
 	BROWSER=$1
 fi
